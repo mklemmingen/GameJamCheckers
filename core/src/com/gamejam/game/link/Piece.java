3814854782f960242ba0b045eda4579c226027c6
mklemmingen <104225647+mklemmingen@users.noreply.github.com>
2024-11-18 09:49:49 +0100

    Push of Framework Chess to publicly accesible Repo. Full Project structure
    Signed-off-by: mklemmingen <104225647+mklemmingen@users.noreply.github.com>

---
+++ core/src/com/gamejam/game/link/Piece.java
@@ -0,0 +1,10 @@
+package com.gamejam.game.link;
+
+public enum Piece {
+    /*
+     * This enum is used to store the different types of pieces that can be used in the game
+     */
+
+    CHECKERSTONE, // moves diagonally forward on square at a time
+    STONE // obstacle
+}
